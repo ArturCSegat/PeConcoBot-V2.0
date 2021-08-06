d1989e5225142d806d28847054d780d52ba7764e
kfh83 <85626296+kfh83@users.noreply.github.com>
2021-08-05 22:59:46 -0300

    caralho

--- peConcoHost.py
+++ peConcoHost.py
@@ -86,6 +86,8 @@ async def on_message(message): # isso é executado toda vez q uma msg é enviada
     # Essa linha é muito importante n apague ela quebra o bot
     if "teresa" in message.content.lower():
         await message.channel.send("É com z")
+    if "É com z" in  message.content.lower and message.channel.id == "869758343873441822"():
+        await message.channel.send("É com z")
     await bot.process_commands(message)
 #only commands from here
 
